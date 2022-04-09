19c35fb6afe72bd75a5298c030c03034fe05e53b
Gyanishiv <shivsppawar@gmail.com>
2022-04-10 04:38:57 +0530

    add

---
+++ Assets/Script/skyboxManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skyboxManager : MonoBehaviour
+{
+    public void skyboxChanger(Material material){
+        RenderSettings.skybox = material;
+    }
+}
